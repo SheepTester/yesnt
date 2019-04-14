4d0ecacdaf4655cb9f376dcca7f67edbaa4dc9e7
SheepTester <ussa.say@gmail.com>
2019-04-14 14:01:04 -0700

    fancy lamp models

--- room-setup.js
+++ room-setup.js
@@ -10,28 +10,41 @@ function createMat(x, z) {
 function setupRoom(scene) {
   const floor = new THREE.Mesh(
     new THREE.PlaneBufferGeometry(1000, 1000),
-    new THREE.MeshPhysicalMaterial({color: 0xF1C38E, roughness: 0.6, metalness: 0.2})
+    new THREE.MeshStandardMaterial({color: 0xF1C38E, roughness: 0.6, metalness: 0.2})
   );
   floor.rotation.x = -Math.PI / 2;
   scene.add(floor);
 
-  const lampSphere = new THREE.Mesh(
-    new THREE.SphereBufferGeometry(3),
-    new THREE.MeshStandardMaterial({emissive: 0xffd6aa, roughness: 0.6, metalness: 0.2})
-  );
-  lampSphere.position.set(0, 10, -450);
-  scene.add(lampSphere);
-
-  const lamp = new THREE.PointLight(0xffd6aa, 0.5);
-  lamp.position.set(0, 10, -450);
-  scene.add(lamp);
+  // const lampSphere = new THREE.Mesh(
+  //   new THREE.SphereBufferGeometry(3),
+  //   new THREE.MeshStandardMaterial({emissive: 0xffd6aa, roughness: 0.6, metalness: 0.2})
+  // );
+  // lampSphere.position.set(0, 10, -450);
+  // scene.add(lampSphere);
+  //
+  // const lamp = new THREE.PointLight(0xffd6aa, 0.8);
+  // lamp.position.set(0, 10, -450);
+  // scene.add(lamp);
 
   const frontWall = new THREE.Mesh(
     new THREE.PlaneBufferGeometry(1000, 200),
-    new THREE.MeshPhysicalMaterial({color: 0xffffff, roughness: 0.6, metalness: 0.4})
+    new THREE.MeshStandardMaterial({color: 0xffffff, roughness: 0.6, metalness: 0.4})
   );
   frontWall.position.set(0, 50, -500);
   scene.add(frontWall);
 
-  scene.add(createMat(10, -450));
+  for (let x = 0; x < 21; x++) {
+    for (let z = 0; z < 6; z++) {
+      scene.add(createMat((x - 10) * 15, -450 + z * 25));
+    }
+  }
+
+  new THREE.ObjectLoader().load('./models/lamp.json', lampModel => {
+    const lamp = lampModel.clone();
+    lamp.position.set(50, 0, -480);
+    scene.add(lamp);
+    const lamp2 = lampModel.clone();
+    lamp2.position.set(-80, 0, -480);
+    scene.add(lamp2);
+  });
 }
